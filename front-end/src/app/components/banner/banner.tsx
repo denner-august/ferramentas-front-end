eaff0d0122bf5b4946f06e580bfdf421d412a7c3
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-03-21 18:57:02 -0300

    Update banner.tsx

--- front-end/src/app/components/banner/banner.tsx
+++ front-end/src/app/components/banner/banner.tsx
@@ -1,7 +1,35 @@
+import Image from "next/image";
+import styles from "./banner.module.scss";
+import PrincipalImage from "@/../public/image/principal/header.jpg";
+
 export function Banner() {
   return (
-    <>
-      <p>banner</p>
-    </>
+    <div className={styles.Container}>
+      <div className={styles.apresenta}>
+        <h1>
+          Melhore suas habilidades de codificação de front-end criando projetos
+          reais
+        </h1>
+
+        <p>
+          Resolva desafios reais de HTML, CSS e JavaScript enquanto trabalha com
+          designs profissionais. Aqui você Encontrara diversos projetos de
+          diferentes niveis para Praticar e melhorar ainda mais seu nivel de
+          codificação
+        </p>
+      </div>
+
+      <figure>
+        <Image
+          src={PrincipalImage}
+          alt="como podemos te ajudar?"
+          style={{
+            width: "100%",
+            height: "auto",
+          }}
+          blurDataURL={`${PrincipalImage}`}
+        />
+      </figure>
+    </div>
   );
 }
