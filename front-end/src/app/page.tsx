396ffe1189c420b7fd2872389d77f649255ff728
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-03-21 18:56:57 -0300

    Update page.tsx

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Banner } from "./components/banner/banner";
 import { Header } from "./components/header/header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Banner />
     </>
   );
 }
