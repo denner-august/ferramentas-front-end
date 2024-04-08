88ad615a2f7b8c5e7ee076ef90484ea0fa1f9202
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-04-08 17:57:32 -0300

    Update page.tsx

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,6 +1,8 @@
+import "react-toastify/dist/ReactToastify.css";
 import { Banner } from "./components/banner/banner";
 import { CursoProgramação } from "./components/cursos/programação/cursoProgramação";
 import { Header } from "./components/header/header";
+import { ToastContainer } from "react-toastify";
 
 export default function Home() {
   return (
@@ -8,6 +10,7 @@ export default function Home() {
       <Header />
       <Banner />
       <CursoProgramação />
+      <ToastContainer />
     </>
   );
 }
