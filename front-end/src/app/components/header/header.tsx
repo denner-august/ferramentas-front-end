55157fb494d7bc4b7da841fd4a59c4623cb00b24
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-04-08 17:57:38 -0300

    Update header.tsx

--- front-end/src/app/components/header/header.tsx
+++ front-end/src/app/components/header/header.tsx
@@ -1,7 +1,23 @@
+"use client";
 import styles from "./header.module.scss";
 import { RenderLinks } from "@/app/functions/renderLinks";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 
 export function Header() {
+  useEffect(() => {
+    toast("Este projeto ainda está em desenvolvimento", {
+      position: "top-right",
+      autoClose: 10000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  });
+
   return (
     <header className={styles.header}>
       <h1>Ferramentas front-end</h1>
